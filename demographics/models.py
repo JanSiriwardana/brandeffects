e8b4fa50772cc3c70001151edb8dbc9c34d97943
Theodore Turocy <ted.turocy@gmail.com>
2021-03-30 11:51:40 +0100

    Add standard demographics app.

---
+++ demographics/models.py
@@ -0,0 +1,94 @@
+from otree.api import (
+    models,
+    widgets,
+    BaseConstants,
+    BaseSubsession,
+    BaseGroup,
+    BasePlayer,
+)
+
+
+author = 'Prachi Hejib'
+
+doc = """
+Demographics Survey
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'aboutyou'
+    players_per_group = None
+    num_rounds = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    gender = models.IntegerField(
+        label="What gender do you identify as?",
+        choices=[
+            [1, "Male"],
+            [2, "Female"],
+            [3, "Other"],
+            [4, "Prefer not to say"]
+        ],
+        widget=widgets.RadioSelect,
+        blank=True,
+    )
+
+    age = models.IntegerField(
+        label="How old are you?",
+        blank=True,
+    )
+
+    countryborn = models.StringField(
+        label="Which country (or countries) were you a citizen of when you were born?",
+        blank=True,
+    )
+
+    countrynow = models.StringField(
+        label="In which country is your current permanent residence? "
+              "(If you are in the UK on a student visa, this is where 'home' is.)",
+        blank=True,
+    )
+
+    department = models.StringField(
+        label="In which School are you currently enrolled? (For example, BIO, ECO, EDU, ...)",
+        blank=True,
+    )
+
+    degree = models.IntegerField(
+        label="What type of degree course are you currently enrolled on?",
+        choices=[
+            [1, 'INTO'],
+            [2, 'Bachelor'],
+            [3, 'PG Diploma'],
+            [4, 'Master'],
+            [5, 'PhD'],
+            [6, 'I am a member of staff'],
+            [7, 'Other degree course or affiliation'],
+            [8, 'Prefer not to say']
+        ],
+        widget=widgets.RadioSelect,
+        blank=True,
+    )
+
+    timeuea = models.IntegerField(
+        label="How long have you been at UEA?",
+        choices=[
+            [1, "This is my first year"],
+            [2, "This is my second year"],
+            [3, "This is my third year"],
+            [4, "This is my fourth year"],
+            [5, "I have been here more than four years"],
+            [6, "Prefer not to say"]
+            ],
+        widget=widgets.RadioSelect,
+        blank=True,
+    )
---
+++ demographics/pages.py
@@ -0,0 +1,54 @@
+from ._builtin import Page
+
+
+class Introduction(Page):
+    pass
+
+
+class QuestionPage(Page):
+    template_name = "demographics/GeneralDemographics.html"
+    form_model = 'player'
+
+
+class Page1(QuestionPage):
+    form_fields = ['gender']
+
+
+class Page2(QuestionPage):
+    form_fields = ['age']
+
+
+class Page3(QuestionPage):
+    form_fields = ['countryborn']
+
+
+class Page4(QuestionPage):
+    form_fields = ['countrynow']
+
+
+class Page5(QuestionPage):
+    form_fields = ['degree']
+
+
+class Page6(QuestionPage):
+    form_fields = ['department']
+
+    def is_displayed(self):
+        # Only display if participant responded and gave a degree course
+        return self.player.degree is not None and self.player.degree <= 5
+
+
+class Page7(QuestionPage):
+    form_fields = ['timeuea']
+
+
+page_sequence = [
+    Introduction,
+    Page1,
+    Page2,
+    Page3,
+    Page4,
+    Page5,
+    Page6,
+    Page7,
+]
