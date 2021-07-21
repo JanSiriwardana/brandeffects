8c6ca00d3c79098e230d3acfc376d456ab99c091
Jan Siriwardana <73550706+JanSiriwardana@users.noreply.github.com>
2021-07-21 10:45:26 +0100

    Round 4 attributes populated

--- round_4/models.py
+++ round_4/models.py
@@ -20,8 +20,21 @@ Your app description
 class Constants(BaseConstants):
     name_in_url = 'round_4'
     players_per_group = None
-    num_rounds = 1
+    num_rounds = 3
 
+    beer_price = [1, 2, 3]
+    movie_price = [4, 5, 6]
+    broadband_price = [7, 8, 9]
+
+    ABV = ['A', 'B', 'C']
+    year_of_release = ['D', 'E', 'F']
+    speed = ['G', 'H', 'I']
+    container = ['J', 'K', 'L']
+    genre = ['M', 'N', 'O']
+    contract_length = ['P', 'Q', 'R']
+    volume = ['S', 'T', 'U']
+    rating = ['V', 'W', 'X']
+    data_cap = ['Y', 'Z', '1']
 
 class Subsession(BaseSubsession):
     pass
@@ -32,4 +45,4 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    choice_1 = models.IntegerField(label="Your choice")
+    choice = models.IntegerField
--- round_4/pages.py
+++ round_4/pages.py
@@ -7,8 +7,33 @@ class Decision(Page):
     template_name = "round_4/Decision.html"
 
     def vars_for_template(self):
+        beer_price = Constants.beer_price[self.round_number - 1]
+        movie_price = Constants.movie_price[self.round_number - 1]
+        broadband_price = Constants.broadband_price[self.round_number - 1]
+        ABV = Constants.ABV[self.round_number - 1]
+        year_of_release = Constants.year_of_release[self.round_number - 1]
+        speed = Constants.speed[self.round_number - 1]
+        container = Constants.container[self.round_number - 1]
+        genre = Constants.genre[self.round_number - 1]
+        contract_length = Constants.contract_length[self.round_number - 1]
+        volume = Constants.volume[self.round_number - 1]
+        rating = Constants.rating[self.round_number - 1]
+        data_cap = Constants.data_cap[self.round_number - 1]
         return {'title': "A bit about you...",
+                'beer_price': beer_price,
+                'movie_price': movie_price,
+                'broadband_price': broadband_price,
+                'ABV': ABV,
+                'year_of_release': year_of_release,
+                'speed': speed,
+                'container': container,
+                'genre': genre,
+                'contract_length': contract_length,
+                'volume': volume,
+                'rating': rating,
+                'data_cap': data_cap,
                 'button_text': "Next"}
+
 
 class Results(Page):
     pass
